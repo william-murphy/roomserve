045ec6ac1437a063532b325f4a7d277a7e2b5db5
Will <wpm517@gmail.com>
2023-06-01 23:38:40 -0400

    added code to connect to database and create table, and updated env example with more fields

--- main.go
+++ main.go
@@ -2,8 +2,37 @@ package main
 
 import (
 	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
+type Room struct {
+	ID       uint `gorm:"primaryKey"`
+	Name     string
+	Capacity uint
+}
+
 func main() {
-	fmt.Println("Hello world!")
+	err := godotenv.Load()
+	if err != nil {
+		panic("Error loading .env file")
+	}
+
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", dbHost, dbUser, dbPassword, dbName, dbPort)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("Couldn't connect to database.")
+	}
+
+	db.AutoMigrate(&Room{})
+	// at this point there should be a Room table in the database
 }
